34888b3b86292d87b3c43d4239f0b5342cb5f0a3
laura <yayuezhou2020@gmail.coom>
2024-05-01 14:50:50 +0100

    add toggle dark mode

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <aside>
           <SearchBar/>
           <TagsFilter/>
-          <DarkModeButton/>
+          <DarkModeButton isDark={isDark} toggleDark={toggleDark}/>
         </aside>
       </section>
       
@@ -73,21 +73,17 @@ function DarkModeButton({isDark, toggleDark}){
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
   </svg>
 
-  const lightIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+  const lightIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
   </svg>
 
   return(
-  
-      <button class="w-20 h-10 rounded-full bg-white items-center transition duration-300 focus:outline-none shadow"
+      <button className="w-20 h-10 rounded-full bg-white items-center transition duration-300 focus:outline-none shadow "
           onClick={toggleDark}>
-          <div class="w-12 h-12 relative rounded-full transition duration-500 transform bg-yellow-500 -translate-x-2 p-1 text-white">
+          <div className= {`w-12 h-12 relative rounded-full transition duration-500 transform bg-yellow-500 -translate-x-2 p-1 text-white ${isDark ? 'bg-gray-700': 'bg-yellow-500'} ${isDark ? 'translate-x-full': '-translate-x-2'}`}>
               {isDark ? darkIcon : lightIcon}
           </div>
       </button>
-      
-
-  
   ) 
 }
 
